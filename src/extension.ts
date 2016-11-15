a1d7f4cdd1c41d10cc2847f6a02fdba990e19417
高畅 <gaochang1@kingsoft.com>
2016-11-15 15:42:13 +0800

    支持从选中文字中解析tabfile的Header和Type，生成代码

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,6 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as fs from 'fs';
-import * as lineReader from 'line-reader';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -25,24 +24,228 @@ export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(disposable);
 
-    let contentDir = "src/Content"
+    let contentDir = "src\\Content"
     let gamedataDir = "src/Content/GameData/"
     let generateHeaderDisposable = vscode.commands.registerTextEditorCommand('extension.generateHeader', (textEditor, edit, args) => {
-        let selectString = textEditor.document.getText(textEditor.selection)
-        console.log(selectString)
-        let tabfileClassPath : string = args.path
+        let document = textEditor.document
+        // Get selected string as tabfile path
+        let selectString = document.getText(textEditor.selection)
+        selectString = selectString.trim()
+        if (selectString.startsWith("\"")) {
+            selectString = selectString.substring(1, selectString.length)
+        }
+        if (selectString.endsWith("\"")) {
+            selectString = selectString.substring(0, selectString.length - 1)
+        }
+        // Combine a tabfile from tabfile class file path
+        let tabfileClassPath : string = args.fsPath
         let absPathPrefixIdx = tabfileClassPath.indexOf(contentDir)
-        console.log(absPathPrefixIdx)
         let absPathPrefix = tabfileClassPath.substr(0, absPathPrefixIdx)
-        let absTabfilePath = absPathPrefix.concat(gamedataDir).concat(selectString)
-        console.log(absTabfilePath)
-        let tabfileData = fs.readFileSync(absTabfilePath, "utf-8")
-        console.log(textEditor)
-        console.log(edit)
-        console.log(args)
-        vscode.window.showInformationMessage("Hello");
+        let absTabfilePath = absPathPrefix + gamedataDir + selectString
+        // Find insert position for generated codes
+        let totalText = GetTotalTextInDocument(document)
+        let parseLineIdx = totalText.search(/[\n\r]function.*:OnParseLine\(Parser\)/)
+        if (parseLineIdx != -1) {
+            let insertLine = document.positionAt(parseLineIdx).line + 2
+            ParseTabfileColumnNamesAndTypes(absTabfilePath, edit, insertLine)
+        }
+        else {
+            vscode.window.showErrorMessage("Cannot find a suitable position to insert the generated codes.")
+        }
     });
     context.subscriptions.push(generateHeaderDisposable);
+}
+
+function GetTotalTextInDocument(document : vscode.TextDocument) {
+    let lastLineLen = document.lineAt(document.lineCount - 1).text.length
+    let totalRange = new vscode.Range(0, 0, document.lineCount, lastLineLen)
+    totalRange = document.validateRange(totalRange)
+    let totalText = document.getText(totalRange)
+    return totalText
+}
+
+function ParseTabfileColumnNamesAndTypes(absTabfilePath : string, edit : vscode.TextEditorEdit, insertLine : number) {
+    if (fs.existsSync(absTabfilePath)) {
+        // Read tabfile and split to lines
+        let tabfile = fs.readFileSync(absTabfilePath, "utf-8")
+        let tabfileLines = tabfile.split(/\r\n|\n/)
+        let columnTypes : string[] = null
+        let columnNames : string[] = null
+        let lineIdx = 0
+        // Parse header names and type infos
+        for (; lineIdx < tabfileLines.length; lineIdx++) {
+            let line = tabfileLines[lineIdx];
+            if (line.startsWith("#type")) {
+                let tmpLine = line.replace(/ /g, "").trim()
+                tmpLine = tmpLine.replace(/#type=/, "")
+                columnTypes = tmpLine.split("\t")
+            }
+            else if (columnNames == null && !line.startsWith("#")) {
+                columnNames = line.split("\t")
+            }
+            if (columnNames != null && columnTypes != null) {
+                ParseTabfileCompleted(edit, insertLine, columnNames, columnTypes)
+                break
+            }
+        }
+        if (lineIdx == tabfileLines.length) {
+            ParseTabfileCompleted(edit, insertLine, columnNames, columnTypes)
+        }
+    }
+    else {
+        vscode.window.showErrorMessage("File not exist:" + absTabfilePath)
+    }
+}
+
+function ParseTabfileCompleted(edit : vscode.TextEditorEdit, insertLine : number,  columnNames : string[], columnTypes : string[]) {
+    let generateCode = GenerateLineParseCode(columnNames, columnTypes)
+    edit.insert(new vscode.Position(insertLine, 0), generateCode)
+}
+
+function GenerateLineParseCode(columnNames : string[], columnTypes : string[]) {
+    if (columnNames == null) {
+        vscode.window.showErrorMessage("Cannot find column names.")
+        return
+    } 
+    else if(columnTypes == null) {
+        vscode.window.showWarningMessage("Cannot find column types.")
+    }
+    else if (columnNames.length != columnTypes.length) {
+        vscode.window.showWarningMessage("Column name count isn't equal to column type count. " + columnNames.length + " vs " + columnTypes.length)
+    }
+
+    let generateCode : string[] = []
+    let generateLineIdx = 0
+    generateCode[generateLineIdx++] = "local NewTemplate = {}"
+    for (let i = 0; i < columnNames.length; i++) {
+        let columnName = columnNames[i]
+        let columnType = (columnTypes != null && i < columnTypes.length) ? columnTypes[i] : ""
+        // add type symbol
+        let lineParser = "NewTemplate." + GetColumnTypePrefix(columnType)
+        // convert low_case_with_underline to PascalCase
+        let upperColumnName = columnName.replace(/\_[a-z]/g, function(substr, args) {
+            return substr[1].toLocaleUpperCase()
+        }).replace(/^[a-z]/, function(substr, args) {
+            return substr.toLocaleUpperCase()
+        })
+        lineParser = lineParser + upperColumnName + " = Parser:Get(\"" + columnName + "\", "
+        lineParser = lineParser + GetDefaultValueByColumnType(columnType) + ", "
+        lineParser = lineParser + GetParserType(columnType) + ", true)"
+        generateCode[generateLineIdx++] = lineParser
+    }
+    AddIntentSpace(generateCode, 4)
+    let nextStartIdx = AlignWithCharacter(generateCode, 1, "=", 0)
+    nextStartIdx = AlignWithCharacter(generateCode, 1, ",", nextStartIdx)
+    nextStartIdx = AlignWithCharacter(generateCode, 1, ",", nextStartIdx, true)
+    nextStartIdx = AlignWithCharacter(generateCode, 1, ",", nextStartIdx)
+    return TextLinesToEntireText(generateCode)
+}
+
+let IntentSpaceMap : string[] = []
+IntentSpaceMap[0] = " "
+
+function GetIntentSpaceString(space : number) {
+    let bit = 0
+    let retStr = ""
+    while (space > 0) {
+        if (IntentSpaceMap[bit] == null || IntentSpaceMap[bit].length == 0) {
+            IntentSpaceMap[bit] = IntentSpaceMap[bit - 1] + IntentSpaceMap[bit - 1]
+        }
+        if (space % 2) {
+            retStr += IntentSpaceMap[bit]
+            --space
+        }
+        ++bit
+        space /= 2
+    }
+    return retStr
+}
+
+function AlignWithCharacter(textLines : string[], startLine : number, character : string, startIdx : number, rightAlign : boolean = false) {
+    let rightMostIdx = 0
+    for (let i = startLine; i < textLines.length; ++i) {
+        let characterIdx = textLines[i].indexOf(character, startIdx)
+        rightMostIdx = characterIdx > rightMostIdx ? characterIdx : rightMostIdx
+    }
+    for (let i = startLine; i < textLines.length; ++i) {
+        let line = textLines[i]
+        let characterIdx = line.indexOf(character, startIdx)
+        let intentStr = GetIntentSpaceString(rightMostIdx - characterIdx)
+        if (rightAlign) {
+            characterIdx = line.lastIndexOf(" ", characterIdx)
+        }
+        else {
+            characterIdx += character.length;
+        }
+        let front = line.substring(0, characterIdx)
+        let rear = line.substring(characterIdx, line.length)
+        textLines[i] = front + intentStr + rear
+    }
+    return rightMostIdx + 1
+}
+
+function TextLinesToEntireText(textLines : string[]) {
+    let retText = ""
+    for (let i = 0; i < textLines.length; ++i) {
+        retText += textLines[i] + "\r\n"
+    }
+    return retText
+}
+
+function AddIntentSpace(textLines : string[], space : number) {
+    let intentStr = GetIntentSpaceString(space)
+    for (let i = 0; i < textLines.length; ++i) {
+        let line = textLines[i]
+        textLines[i] = intentStr + line
+    }
+}
+
+function GetParserType(columnType : string) {
+    switch (columnType) {
+        case "int":
+            return "Parser.TypeInt"
+        case "float":
+            return "Parser.TypeFloat"
+        case "string":
+            return "Parser.TypeString"
+        case "bool":
+            return "Parser.TypeBool"
+        case "loc":
+            return "Parser.TypeL10N"
+    }
+    return "\"\""
+}
+
+function GetDefaultValueByColumnType(columnType : string) {
+    switch (columnType) {
+        case "int":
+            return "-1"
+        case "float":
+            return "0.0"
+        case "string":
+            return "\"\""
+        case "bool":
+            return "false"
+        case "loc":
+            return "\"\""
+    }
+    return "\"\""
+}
+
+function GetColumnTypePrefix(columnType : string) {
+    switch (columnType) {
+        case "int":
+            return "n"
+        case "float":
+            return "f"
+        case "string":
+            return "sz"
+        case "bool":
+            return "b"
+        case "loc":
+            return "loc"
+    }
+    return "var"
 }
 
 // this method is called when your extension is deactivated
